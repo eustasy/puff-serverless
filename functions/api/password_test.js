7256d9d2e53ee29ff877d81089fd4c353e993e7a
Lewis Goddard <lewis.goddard@eustasy.org>
2023-10-22 16:23:25 +0100

    Update password_test.js

--- functions/api/password_test.js
+++ functions/api/password_test.js
@@ -1,6 +1,12 @@
 export function onRequest(context) {
   const { searchParams } = new URL(context.request.url)
   let pw = searchParams.get('pw')
-  if ( pw.length >= 12 ) {}
-  return new Response(`${pw} ${pw.length}`)
+  var response_html = '<div id="pw-checklist"><h3>Password Requirements</h3><ul>'
+  if ( pw.length >= 12 ) {
+    response_html += '<li style="color:#064e3b"><strong>Must</strong> be at least 12 character long</li>'
+  } else {
+    response_html += '<li style="color:#7f1d1d"><strong>Must</strong> be at least 12 character long</li>'
+  }
+  response_html += '</ul></div>'
+  return new Response(response_html)
 }
