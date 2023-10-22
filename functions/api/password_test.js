7a8f1620e532ee97f2dae90a366fee6831facea8
Lewis Goddard <lewis.goddard@eustasy.org>
2023-10-22 17:24:58 +0100

    Update password_test.js

--- functions/api/password_test.js
+++ functions/api/password_test.js
@@ -37,8 +37,8 @@ export async function onRequest(context) {
     await fetch('https://api.pwnedpasswords.com/range/' + pw_sha1_f5)
     .then((response) => response.text())
     .then((text) => {
-    response_html += '<li>' + data + '</li>'
-      var inputArray = data.split('\n')
+      response_html += '<li>' + text + '</li>'
+      var inputArray = text.split('\n')
       for (var i = 0; i < inputArray.length; i++) {
         let line_f35 = inputArray[i].slice(0, 35)
         response_html += '<li>' + i + ' : ' + line_f35 + '</li>'
