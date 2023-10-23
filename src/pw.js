8ef16b3819b4317a72a55f00d5146f25cf589ef9
Lewis Goddard <lewis.goddard@eustasy.org>
2023-10-23 12:19:10 +0100

    Create pw.js

---
+++ src/pw.js
@@ -0,0 +1,15 @@
+export async function pwcheck(pw) {
+  var result = true
+  if ( pw.length < 12 ) {
+    result = false
+  }
+  var hasNumber = /\d/
+  if ( !hasNumber.test(pw) ) {
+    result = false
+  }
+  var hasSpecial = /[!-\/:-@[-`{-~]/
+  if ( !hasSpecial.test(pw) ) {
+    result = false
+  }
+  return new Response(result)
+}
