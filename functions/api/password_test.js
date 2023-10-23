5ccb8ae2518f511f890fb9ad9c5015ed8734284c
Lewis Goddard <lewis.goddard@eustasy.org>
2023-10-23 12:30:53 +0100

    Update password_test.js

--- functions/api/password_test.js
+++ functions/api/password_test.js
@@ -3,6 +3,10 @@ import { password_requirements } from './../../src/passwords.js';
 export async function onRequest(context) {
   const { searchParams } = new URL(context.request.url)
   let pw = searchParams.get('pw')
-  var response_html = await password_requirements(pw)
+  try {
+    var response_html = await password_requirements(pw)
+  } catch (err) {
+      response_html = err
+  }
   return new Response(response_html)
 }
