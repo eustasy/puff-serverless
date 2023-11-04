f48ced2af91bd8ed0d4f128ddf7326b6d636bd0d
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-04 17:26:19 +0000

    Create uuid_test.js

---
+++ functions/api/uuid_test.js
@@ -0,0 +1,6 @@
+import { generate_uuidv4 } from './../../src/uuids.js';
+  
+export async function onRequest(context) {
+  let response_html = await generate_uuidv4()
+  return new Response(response_html)
+}
