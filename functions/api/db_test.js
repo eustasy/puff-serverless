e3dd1a99c2a0874771ad79b65c00eceb339a2639
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-04 20:18:06 +0000

    Update db_test.js

--- functions/api/db_test.js
+++ functions/api/db_test.js
@@ -6,7 +6,7 @@ export async function onRequest(context) {
   //let response_html = await password_check(pw)
   // Create a prepared statement with our query
   const ps = context.env.DATABASE.prepare('SELECT * from users')
-  const data = await ps.first()
+  const data = await ps.all()
 
   return Response.json(data)
 }
