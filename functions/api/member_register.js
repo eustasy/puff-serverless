b066685790c56e71d89e8608609efa497ed754b9
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-04 20:16:28 +0000

    Update member_register.js

--- functions/api/member_register.js
+++ functions/api/member_register.js
@@ -6,8 +6,8 @@ export async function onRequest(context) {
   let name = searchParams.get('name')
   const uuid = await generate_uuidv4()
   // Step 1. Register the member
-  const { duration } = (await context.env.DATABASE.prepare('INSERT INTO users (uuid, name) VALUES (?1, ?2)').bind(uuid, name).run()).meta
+  const info = await context.env.DATABASE.prepare('INSERT INTO users (uuid, name) VALUES (?1, ?2)').bind(uuid, name).run()
   // Step 2. Register the email
   // Step 3. Register the password
-  return new Response(duration)
+  return Response.json(info)
 }
