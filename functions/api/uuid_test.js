6790e4482ce30b0511de6ff83801dc7b45a2b9da
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 19:39:55 +0000

    Update uuid_test.js

--- functions/api/uuid_test.js
+++ functions/api/uuid_test.js
@@ -1,6 +1,4 @@
-import { generate_uuidv4 } from "./../../src/uuids.js"
-
 export async function onRequest(context) {
-  let response_html = await generate_uuidv4()
+  let response_html = await crypto.randomUUID()
   return new Response(response_html)
 }
