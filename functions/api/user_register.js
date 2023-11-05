a3b8e685fe5250daf6516344572ac3e1aba44147
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 20:12:49 +0000

    Update user_register.js

--- functions/api/user_register.js
+++ functions/api/user_register.js
@@ -1,11 +1,10 @@
 import { user_register } from "./../../src/users.js"
 
 export async function onRequest(context) {
-  // Step 0. Prep work
   const formdata = await context.request.formData()
-  const email = formdata.get("email")
-  const name = formdata.get("name")
-  const pw = formdata.get("pw")
-  const results = user_register(context, name, email, pw)
+  const email = await formdata.get("email")
+  const name = await formdata.get("name")
+  const pw = await formdata.get("pw")
+  const results = await user_register(context, name, email, pw)
   return new Response(results)
 }
