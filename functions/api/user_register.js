146ddf09a94248fe7a31d0c082e0b59b59d796a4
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 18:57:28 +0000

    Update user_register.js

--- functions/api/user_register.js
+++ functions/api/user_register.js
@@ -1,4 +1,4 @@
-import { generate_uuidv4 } from './../../src/uuids.js';
+import { puff_hashing_password } from './../../src/hashing.js';
   
 export async function onRequest(context) {
   // Step 0. Prep work
@@ -7,7 +7,7 @@ export async function onRequest(context) {
   // TODO Check the email isn't already registered
   
   // Step 1. Register the user
-  const uuid = await generate_uuidv4()
+  const uuid = await crypto.randomUUID()
   const name = formdata.get('name')
   const insert_user = await context.env.DATABASE.prepare('INSERT INTO users (user_uuid, user_name) VALUES (?1, ?2)').bind(uuid, name).run()
   
@@ -16,10 +16,15 @@ export async function onRequest(context) {
   
   // Step 3. Register the password
   const pw = formdata.get('pw')
+  const { hash, salt } = await puff_hashing_password(pw)
+  const secret_value = hash + ':' + salt
+  const now = new Date(Date.now()).toISOString()
+  const insert_secret = await context.env.DATABASE.prepare('INSERT INTO secrets (user_uuid, secret_type, secret_value, secret_created_at) VALUES (?1, ?2)').bind(uuid, 'SHA-384', secret_value, now).run()
 
   let results = {
-      ...insert_user,
-      ...insert_email
+    ...insert_user,
+    ...insert_email,
+    ...insert_secret
   }
   return Response.json(results)
 }
