a2c7e431e51d06c11b2805fa18b61b850716f3b9
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 15:26:21 +0000

    Update post_test.js

--- functions/api/post_test.js
+++ functions/api/post_test.js
@@ -10,8 +10,8 @@ const getMethods = (obj) => {
 }
 
 export async function onRequest(context) {
-  //const formdata = getMethods(context.request)
-  const formdata = await context.request.formData()
+  const formdata = getMethods(context.request)
+  //const formdata = await context.request.formData()
   //let pw = formdata.get('pw')
   //let response_html = await password_requirements(pw)
   return Response.json(formdata)
