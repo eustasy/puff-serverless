e7c9898964710285b7c438f9f7a1f976adb44800
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 16:25:52 +0000

    Update user_register.js

--- functions/api/user_register.js
+++ functions/api/user_register.js
@@ -2,19 +2,20 @@ import { generate_uuidv4 } from './../../src/uuids.js';
   
 export async function onRequest(context) {
   // Step 0. Prep work
-  const { searchParams } = new URL(context.request.url)
-  let email = searchParams.get('email')
+  const formdata = (await context.request.formData())
+  const email = formdata.get('email')
   // TODO Check the email isn't already registered
   
   // Step 1. Register the user
-  let name = searchParams.get('name')
   const uuid = await generate_uuidv4()
+  const name = formdata.get('name')
   const insert_user = await context.env.DATABASE.prepare('INSERT INTO users (user_uuid, user_name) VALUES (?1, ?2)').bind(uuid, name).run()
   
   // Step 2. Register the email
   const insert_email = await context.env.DATABASE.prepare('INSERT INTO emails (user_uuid, email_address) VALUES (?1, ?2)').bind(uuid, email).run()
   
   // Step 3. Register the password
+  const pw = formdata.get('pw')
 
   let results = {
       ...insert_user,
