d1b5d2aa78b2815e2b8949c8f32544df4b0da20c
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 05:09:24 +0000

    Update hash_tests.js

--- functions/api/hash_tests.js
+++ functions/api/hash_tests.js
@@ -1,26 +1,13 @@
+import { puff_hashing } from './../../src/utilities_hashing.js';
+
 export async function onRequest(context) {
   const pw = "B4c0/\/"
-  const myText = new TextEncoder().encode(pw)
-  const myDigest = await crypto.subtle.digest(
-    {
-      name: 'SHA-384',
-    },
-    myText // The data you want to hash as an ArrayBuffer
-  )
-  const sha384 = new Uint8Array(myDigest).reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '')
-  const myDigest2 = await crypto.subtle.digest(
-    {
-      name: 'SHA-1',
-    },
-    myText // The data you want to hash as an ArrayBuffer
-  )
-  const sha1 = new Uint8Array(myDigest2).reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '')
-  let uuid = crypto.randomUUID()
+  const sha384 = await puff_hashing_password(pw)
+  const sha1 = await puff_hashing_sha1_hibp(pw)
 
   var hashes = {
     'sha384': sha384,
     'sha1': sha1,
-    'uuid': uuid
   }
   return Response.json(hashes)
 }
