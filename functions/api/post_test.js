a5ce7c02c192cf38dac14437b236fcb6559d8bfb
Lewis Goddard <lewis.goddard@eustasy.org>
2023-11-05 15:17:37 +0000

    Update post_test.js

--- functions/api/post_test.js
+++ functions/api/post_test.js
@@ -1,8 +1,17 @@
 import { password_requirements } from './../../src/passwords.js';
-  
+
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
+
 export async function onRequest(context) {
-  const formdata = await context.request.formdata()
-  let pw = formdata.get('pw')
-  let response_html = await password_requirements(pw)
-  return new Response(response_html)
+  const formdata = getMethods(context.request)
+  //let pw = formdata.get('pw')
+  //let response_html = await password_requirements(pw)
+  return Response.json(formdata)
 }
