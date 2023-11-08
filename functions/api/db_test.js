829147861838bc36b63da3740b150ee4cc727dfb
lewisgoddard <lewisgoddard@users.noreply.github.com>
2023-11-08 19:13:52 +0000

    XO Fixes

--- functions/api/db_test.js
+++ functions/api/db_test.js
@@ -1,12 +1,12 @@
-//import { password_check } from './../../src/passwords.js';
+// Import { password_check } from './../../src/passwords.js';
 
 export async function onRequest(context) {
-  //const { searchParams } = new URL(context.request.url)
-  //let pw = searchParams.get('pw')
-  //let response_html = await password_check(pw)
-  // Create a prepared statement with our query
-  const ps = context.env.DATABASE.prepare("SELECT * from users")
-  const data = await ps.all()
+	// Const { searchParams } = new URL(context.request.url)
+	// let pw = searchParams.get('pw')
+	// let response_html = await password_check(pw)
+	// Create a prepared statement with our query
+	const ps = context.env.DATABASE.prepare("SELECT * from users")
+	const data = await ps.all()
 
-  return Response.json(data)
+	return Response.json(data)
 }
--- functions/api/user_register.js
+++ functions/api/user_register.js
@@ -1,10 +1,10 @@
-import { user_register } from "./../../src/users.js"
+import {user_register} from "../../src/users.js"
 
 export async function onRequest(context) {
-  const formdata = await context.request.formData()
-  const email = await formdata.get("email")
-  const name = await formdata.get("name")
-  const pw = await formdata.get("pw")
-  const results = await user_register(context, name, email, pw)
-  return new Response(results)
+	const formdata = await context.request.formData()
+	const email = await formdata.get("email")
+	const name = await formdata.get("name")
+	const pw = await formdata.get("pw")
+	const results = await user_register(context, name, email, pw)
+	return new Response(results)
 }
