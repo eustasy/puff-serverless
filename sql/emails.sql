693d195e36f956cd8d570538dfdba56c02322071
Lewis Goddard <lewis.goddard@eustasy.org>
2025-05-26 02:47:10 +0100

    Update emails.sql

--- sql/emails.sql
+++ sql/emails.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS emails (
     user_uuid STRING NOT NULL,
     email_address STRING PRIMARY KEY,
-    is_verified INTEGER DEFAULT 0, -- Replaces email_verified
+    is_verified INTEGER DEFAULT 0,
     is_primary INTEGER NOT NULL DEFAULT 0,
-    verified_at TEXT DEFAULT NULL -- Added column
+    verified_at TEXT DEFAULT NULL
 );
