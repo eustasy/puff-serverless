4c7bd19e8041efb687bc0f4e2cf39dc32bb9fde9
Lewis Goddard <lewis.goddard@eustasy.org>
2025-05-26 03:49:22 +0100

    Update user_login.js

--- functions/api/user_login.js
+++ functions/api/user_login.js
@@ -4,12 +4,5 @@ export async function onRequest(context) {
   const formdata = await context.request.formData()
   const email = await formdata.get("email")
   const pw = await formdata.get("pw")
-  const results = await user_login(context, email, pw)
-  return new Response(
-    results,
-    { 
-      status: results.error ? 401 : 200,
-      headers: { "Content-Type": "application/json" },
-    }
-  )
+  return await user_login(context, email, pw)
 }
