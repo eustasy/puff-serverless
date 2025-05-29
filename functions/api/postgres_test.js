0738d4447c2f4d33f02ef21b7bb27292313acdad
lewisgoddard <lewisgoddard@users.noreply.github.com>
2025-05-29 04:03:23 +0000

    Prettified Code!

--- functions/api/postgres_test.js
+++ functions/api/postgres_test.js
@@ -1,17 +1,16 @@
 export async function onRequest(context) {
-  const { Client } = require("pg");
-  const client = new Client(context.env.HYPERDRIVE.connectionString);
+  const { Client } = require("pg")
+  const client = new Client(context.env.HYPERDRIVE.connectionString)
 
-  await client.connect();
+  await client.connect()
   try {
-    const results = await client.query("SELECT NOW()");
-    console.log(results);
-    return Response.json(results);
+    const results = await client.query("SELECT NOW()")
+    console.log(results)
+    return Response.json(results)
   } catch (err) {
-    console.error("error executing query:", err);
-    return Response.json({ error: "Failed to execute query" }, { status: 500 });
+    console.error("error executing query:", err)
+    return Response.json({ error: "Failed to execute query" }, { status: 500 })
   } finally {
-    client.end();
+    client.end()
   }
-
 }
