19be919e7c7064c1d7874fa57f32fc48495e1ab3
Lewis Goddard <lewis.goddard@eustasy.org>
2025-05-29 12:13:01 +0100

    Move password apis

--- functions/api/test/password.js
+++ functions/api/test/password.js
@@ -2,6 +2,6 @@ import { password_check } from "../../../src/passwords.js"
 
 export async function onRequest(context) {
   const pw = (await context.request.formData()).get("pw")
-  let response_html = await password_check(pw)
-  return new Response(response_html)
+  let response_bool = await password_check(pw)
+  return new Response(response_bool)
 }
