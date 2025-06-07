f5722e8eff78f701430990747d2db49e898a5cec
Lewis Goddard <lewis.goddard@eustasy.org>
2025-06-07 22:02:34 +0100

    Remove testing nodes, update index.

--- functions/api/test/password.js
+++
@@ -1,7 +0,0 @@
-import { password_requirements } from "../../../src/passwords.js"
-
-export async function onRequest(context) {
-  const pw = (await context.request.formData()).get("pw")
-  let response_bool = await password_requirements(pw)
-  return new Response(response_bool)
-}
--- functions/api/test/uuid.js
+++
@@ -1,4 +0,0 @@
-export async function onRequest(context) {
-  let response_html = await crypto.randomUUID()
-  return new Response(response_html)
-}
