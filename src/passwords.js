b96c4d6d9bfb78486af2e5ecbef85efcc62b4291
Lewis Goddard <lewis.goddard@eustasy.org>
2026-05-12 18:40:47 +0100

    Update passwords.js

--- src/passwords.js
+++ src/passwords.js
@@ -214,21 +214,21 @@ export async function password_requirements_html(pw) {
   //response_html += '<li>' + pw_sha1.f5 + ' : ' + pw_sha1.l35 + '</li>'
   try {
     var compromised = 0
-    await fetch("https://api.pwnedpasswords.com/range/" + pw_sha1.f5)
-      .then((response) => response.text())
-      .then((text) => {
-        //response_html += '<li>' + text + '</li>'
-        var inputArray = text.split("\n")
-        // TODO Minor perf improvement: This for loop always runs the full length, but if we wrap it in a non-exported function we can `return` when we find a match.
-        for (var i = 0; i < inputArray.length; i++) {
-          let line_f35 = inputArray[i].slice(0, 35)
-          let line_ln = inputArray[i].substring(36)
-          //response_html += '<li>' + i + ' : ' + line_f35 + ' : ' + line_ln + '</li>'
-          if (line_f35 == pw_sha1.l35.toUpperCase()) {
-            compromised = parseInt(line_ln)
-          }
-        }
-      })
+    const response = await fetch(
+      "https://api.pwnedpasswords.com/range/" + pw_sha1.f5
+    )
+    const text = await response.text()
+    //response_html += '<li>' + text + '</li>'
+    var inputArray = text.split("\n")
+    // TODO Minor perf improvement: This for loop always runs the full length, but if we wrap it in a non-exported function we can `return` when we find a match.
+    for (var i = 0; i < inputArray.length; i++) {
+      let line_f35 = inputArray[i].slice(0, 35)
+      let line_ln = inputArray[i].substring(36)
+      //response_html += '<li>' + i + ' : ' + line_f35 + ' : ' + line_ln + '</li>'
+      if (line_f35 == pw_sha1.l35.toUpperCase()) {
+        compromised = parseInt(line_ln)
+      }
+    }
 
     if (compromised > 0) {
       response_html +=
